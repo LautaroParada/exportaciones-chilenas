7d27dbd529fb6d663f334d73fd84c7ad32cfab5c
LautaroParada <lautaro.parada.opazo@gmail.com>
2022-07-25 11:54:59 -0400

    Create macro.py
    Codigo inicial

---
+++ macro.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 25 11:28:38 2022
+
+@author: lauta
+"""
+
+import os
+
+from bcch import BancoCentralDeChile
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+# Por seguridad, es mejor guardar las contraseñas y usuarios en las variables de entorno
+bcch_user = os.environ['BCCH_USER']
+bcch_pwd = os.environ['BCCH_PWD']
+
+# Creación de la instancia
+client = BancoCentralDeChile(bcch_user, bcch_pwd)
+
+def cleaner(serie:str, resam:str=None, operations:list=None):
+    """
+    Limpiar la serie proveniente del la APIy dejarla lista para ocupar
+
+    Parameters
+    ----------
+    serie : str
+        id de la serie macro a solicitar.
+    resam : str
+        frecuencia para el resampling.
+    operation : list
+        operación(es) para agregar los datos resampliandos.
+
+    Returns
+    -------
+    pandas DataFrame
+        serie lista para ocupar.
+
+    """
+    serie_ = pd.DataFrame(client.get_macro(serie=serie))
+    serie_['value'] = pd.to_numeric(serie_['value'], errors='coerce')
+    serie_['indexDateString'] = pd.to_datetime(serie_['indexDateString'], format='%d-%m-%Y')
+    serie_.set_index('indexDateString', inplace=True)
+    del serie_['statusCode']
+    
+    if (resam is not None) & (operations is not None):
+        serie_ = serie_.resample(resam).agg(operations)
+        # renombrar las columnas
+        serie_.columns = ['_'.join(x) for x in serie_.columns]
+        return serie_
+    else:
+        return serie_
+
+#%% Exportaciones de los principales productos chilenos
+
+# Solicitar los datos
+exportaciones = cleaner('F068.B1.FLU.Z.0.C.N.Z.Z.Z.Z.6.0.M')
+exp_mineras = cleaner('F068.B1.FLU.A.0.C.N.Z.Z.Z.Z.6.0.M')
+exp_agro = cleaner('F068.B1.FLU.B.0.C.N.Z.Z.Z.Z.6.0.M')
+exp_ind = cleaner('F068.B1.FLU.C.0.C.N.Z.Z.Z.Z.6.0.M')
+
+# Unir todo
+exportaciones = exportaciones.join(exp_mineras).join(exp_agro).join(exp_ind)
+# cambiar los nombres
+exportaciones.columns = ['total_fob', 'mineras', 'agropecuaria', 'industriales']
+
+# Crear porcentajes de cada serie por cada mes
+exportaciones_porcion = exportaciones.divide(exportaciones['total_fob'], axis=0)
