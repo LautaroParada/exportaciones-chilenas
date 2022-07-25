ecce3af40627f1efd6e8a15995e764e816448e9f
LautaroParada <lautaro.parada.opazo@gmail.com>
2022-07-25 12:11:34 -0400

    Categorias principales
    Desglose de las principales exportaciones chilenas.

--- macro.py
+++ macro.py
@@ -57,15 +57,42 @@ def cleaner(serie:str, resam:str=None, operations:list=None):
 #%% Exportaciones de los principales productos chilenos
 
 # Solicitar los datos
-exportaciones = cleaner('F068.B1.FLU.Z.0.C.N.Z.Z.Z.Z.6.0.M')
-exp_mineras = cleaner('F068.B1.FLU.A.0.C.N.Z.Z.Z.Z.6.0.M')
-exp_agro = cleaner('F068.B1.FLU.B.0.C.N.Z.Z.Z.Z.6.0.M')
-exp_ind = cleaner('F068.B1.FLU.C.0.C.N.Z.Z.Z.Z.6.0.M')
+exportaciones = cleaner('F068.B1.FLU.Z.0.C.N.Z.Z.Z.Z.6.0.M', resam='Q', operations=['sum'])
+exp_mineras = cleaner('F068.B1.FLU.A.0.C.N.Z.Z.Z.Z.6.0.M', resam='Q', operations=['sum'])
+exp_agro = cleaner('F068.B1.FLU.B.0.C.N.Z.Z.Z.Z.6.0.M', resam='Q', operations=['sum'])
+exp_ind = cleaner('F068.B1.FLU.C.0.C.N.Z.Z.Z.Z.6.0.M', resam='Q', operations=['sum'])
 
 # Unir todo
-exportaciones = exportaciones.join(exp_mineras).join(exp_agro).join(exp_ind)
+exportaciones = exportaciones.join(exp_mineras, rsuffix='_min').join(exp_agro, rsuffix='_agro').join(exp_ind, rsuffix='_ind')
 # cambiar los nombres
 exportaciones.columns = ['total_fob', 'mineras', 'agropecuaria', 'industriales']
 
 # Crear porcentajes de cada serie por cada mes
-exportaciones_porcion = exportaciones.divide(exportaciones['total_fob'], axis=0)
+exportaciones_porcion = exportaciones.divide(exportaciones['total_fob'], axis=0) * 100
+
+fig, ax = plt.subplots(figsize=(10, 5))
+
+ax.stackplot(
+    exportaciones_porcion.index,
+    exportaciones_porcion['mineras'],
+    exportaciones_porcion['agropecuaria'],
+    exportaciones_porcion['industriales'],
+    alpha=0.5
+    )
+fig.suptitle('Desglose de las exportaciones chilenas', fontweight='bold')
+plt.title('Categorias principales, periodicidad trimestral')
+ax.set_ylabel('Porcentaje respecto a las exportaciones totales (%)')
+ax.legend(['Minería', 'Agropecuario-silvícola y pesquero', 'Industriales'], 
+          loc='lower left')
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+
+ax.text(0.15, -0.12,  
+         "Fuente: Banco Central de Chile   Gráfico: Lautaro Parada", 
+         horizontalalignment='center',
+         verticalalignment='center', 
+         transform=ax.transAxes, 
+         fontsize=8, 
+         color='black',
+         bbox=dict(facecolor='tab:gray', alpha=0.5))
+
+plt.show()
