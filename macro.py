b18a58a1657c0dc4ed5f545b0f0b11e7311c866d
LautaroParada <lautaro.parada.opazo@gmail.com>
2022-07-26 15:24:05 -0400

    Version base bar pie chart
    Arreglos minimos para el grafico. Con esto FUNCIONA!

--- macro.py
+++ macro.py
@@ -368,40 +368,44 @@ plt.title('Último dato reportado')
 plt.show()
 
 from matplotlib.patches import ConnectionPatch
-
+# https://matplotlib.org/stable/gallery/pie_and_polar_charts/bar_of_pie.html#sphx-glr-gallery-pie-and-polar-charts-bar-of-pie-py
 # Seleccionando los datos para el pie chart
-
 
 # make figure and assign axis objects
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(9, 5))
 fig.subplots_adjust(wspace=0)
 
 # pie chart parameters
-overall_ratios = 
-labels = ['Approve', 'Disapprove', 'Undecided']
-explode = [0.1, 0, 0]
+overall_ratios = agropecuario_proporcion.iloc[-1, 1:].to_list()
+labels = agropecuario_proporcion.iloc[-1, 1:].index.to_list()
+explode = [0.1, 0, 0, 0]
 # rotate so that first wedge is split by the x-axis
-angle = -180 * overall_ratios[0]
-wedges, *_ = ax1.pie(overall_ratios, autopct='%1.1f%%', startangle=angle,
+angle = -90.5 * overall_ratios[0]
+wedges, *_ = ax1.pie(overall_ratios, autopct='%0.1f%%', startangle=angle,
                      labels=labels, explode=explode)
 
 # bar chart parameters
-age_ratios = [.33, .54, .07, .06]
-age_labels = ['Under 35', '35-49', '50-65', 'Over 65']
+sector_fruticola = uvas.join(manzanas, rsuffix='_1').join(peras, rsuffix='_2').join(arandanos, rsuffix='_3').join(kiwis, rsuffix='_4').join(ciruelas, rsuffix='_5').join(cerezas, rsuffix='_6').join(paltas, rsuffix='_7')
+sector_fruticola.columns = ['Uvas', 'Manzanas', 'Peras', 'Arandanos', 'Kiwis', 'Ciruelas', 'Cerezas', 'Paltas'] 
+
+sector_fruticola_porcion = sector_fruticola.divide(agropecuario['Frutícola'], axis=0) * 100
+
+age_ratios = sector_fruticola_porcion.iloc[-1, :].to_list()
+age_labels = sector_fruticola_porcion.columns.to_list()
 bottom = 1
 width = .2
 
 # Adding from the top matches the legend.
 for j, (height, label) in enumerate(reversed([*zip(age_ratios, age_labels)])):
     bottom -= height
-    bc = ax2.bar(0, height, width, bottom=bottom, color='C0', label=label,
-                 alpha=0.1 + 0.25 * j)
-    ax2.bar_label(bc, labels=[f"{height:.0%}"], label_type='center')
+    # ARREGLAR A UN DEGRADE
+    bc = ax2.bar(0, height, width, bottom=bottom, color='C0', label=label, alpha=0.1 + (1/len(age_labels)) * j)
+    ax2.bar_label(bc, labels=[f"{round(height, 2)}%"], label_type='center')
 
-ax2.set_title('Age of approvers')
+ax2.set_title('Desglose exportaciones\nsector Frutícula')
 ax2.legend()
 ax2.axis('off')
-ax2.set_xlim(- 2.5 * width, 2.5 * width)
+ax2.set_xlim(- 1 * width, 1 * width)
 
 # use ConnectionPatch to draw lines between the two plots
 theta1, theta2 = wedges[0].theta1, wedges[0].theta2
@@ -411,7 +415,7 @@ bar_height = sum(age_ratios)
 # draw top connecting line
 x = r * np.cos(np.pi / 180 * theta2) + center[0]
 y = r * np.sin(np.pi / 180 * theta2) + center[1]
-con = ConnectionPatch(xyA=(-width / 2, bar_height), coordsA=ax2.transData,
+con = ConnectionPatch(xyA=(-width / 2, 0), coordsA=ax2.transData,
                       xyB=(x, y), coordsB=ax1.transData)
 con.set_color([0, 0, 0])
 con.set_linewidth(4)
@@ -420,7 +424,7 @@ ax2.add_artist(con)
 # draw bottom connecting line
 x = r * np.cos(np.pi / 180 * theta1) + center[0]
 y = r * np.sin(np.pi / 180 * theta1) + center[1]
-con = ConnectionPatch(xyA=(-width / 2, 0), coordsA=ax2.transData,
+con = ConnectionPatch(xyA=(-width / 2, -bar_height+1), coordsA=ax2.transData,
                       xyB=(x, y), coordsB=ax1.transData)
 con.set_color([0, 0, 0])
 ax2.add_artist(con)
