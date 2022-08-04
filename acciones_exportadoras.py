090a5bbf9e7db003884bde069595d50f00579feb
LautaroParada <lautaro.parada.opazo@gmail.com>
2022-08-04 16:25:06 -0400

    Update acciones_exportadoras.py
    se corrijieron los nans y el grafico

--- acciones_exportadoras.py
+++ acciones_exportadoras.py
@@ -247,7 +247,11 @@ for sec in sectors:
     temp_columns = temp_.columns
     # ir por cada columna llenada los NAs con la mediana
     for col in temp_columns:
-        temp_[col] = temp_[col].fillna(temp_[col].median())
+        industrias_empresas[col].replace(
+            to_replace=np.nan, 
+            value=industrias_empresas[col].median(), 
+            inplace=True
+            )
     
     print(f"Listo el sector de {sec}")
     
@@ -264,16 +268,9 @@ r_f = client.get_instrument_ta('US10Y.INDX', function='ema', period=20, filter_=
 industrias_empresas['wacc'] = (r_f + industrias_empresas['beta']*(r_m - r_f)) / 100 # volver a decimal 
 
 #%% Agregando la tasa de crecimiento permanente
-from bcch import BancoCentralDeChile
-# Por seguridad, es mejor guardar las contraseñas y usuarios en las variables de entorno
-bcch_user = os.environ['BCCH_USER']
-bcch_pwd = os.environ['BCCH_PWD']
-
-# Creación de la instancia
-client = BancoCentralDeChile(bcch_user, bcch_pwd)
 
 # PIB, volumen a precios del año anterior encadenado, referencia 2018 (miles de millones de pesos encadenados)
-pib_ = cleaner('F032.PIB.FLU.R.CLP.EP18.Z.Z.0.T').pct_change().rolling(window=40).mean() * 4
+pib_ = cleaner('F032.PIB.FLU.R.CLP.EP18.Z.Z.0.T').pct_change().rolling(window=16).mean() * 4
 
 # Ocupare el promedio a largo plazo de chile
 import statsmodels.api as sm
@@ -294,9 +291,16 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 # Filtrar por las empresas del quintal 60 hacia arriba en market cap (3er quintil)
 mkt_cap_filter = pd.to_numeric(industrias_empresas['mkt_cap'], errors='coerce').quantile(0.6)
+# filtrar las empresas que tengan un roic mayor que la tasa permanente de retorno
+empresas = industrias_empresas
+# empresas = empresas[empresas['wacc']>perpetual_growth_rate]
 
-x = industrias_empresas[industrias_empresas['mkt_cap']>=mkt_cap_filter].mv_bv.values.flatten()
-y = industrias_empresas[industrias_empresas['mkt_cap']>=mkt_cap_filter].roe_r.values.flatten()
+empresas = empresas[empresas['mv_bv']<=10]
+empresas = empresas[empresas['mkt_cap']>=mkt_cap_filter]
+
+
+x = empresas.roic_wacc.values.flatten() * 100
+y = empresas.mv_bv.values.flatten()
 
 modelo = np.poly1d(np.polyfit(x, y, 1))
 r_2 = r2_score(y, modelo(x))
@@ -305,14 +309,17 @@ fig, ax = plt.subplots(figsize=(10, 5))
 
 ax.scatter(x, y, color='tab:blue')
 ax.plot(x, modelo(x), color='tab:orange')
-fig.suptitle('Relación entre PB y el ROE del mercado chileno', fontweight='bold')
-plt.title('Se consideran empresas con cap bursatil mediana hasta alta')
-ax.set_ylabel('ROE-g')
-ax.set_xlabel('MV/BV')
+fig.suptitle('Relación entre MV/BV y el Spread del ROIC del mercado chileno', fontweight='bold')
+plt.title('Se consideran empresas en el 4to y 5to quintil de cap. bursátil')
+ax.set_ylabel('MV/BV')
+ax.set_xlabel('ROIC - WACC (%)')
 # limites de valorización
 ax.axhline(y=np.median(y), color='tab:red', linestyle='dashed')
 ax.axvline(x=np.median(x), color='tab:red', linestyle='dashed')
-ax.text(0.2, 0.8,  
+ax.fill_betweenx(y=np.linspace(np.median(y), np.max(y)), x1=np.min(x), x2=np.median(x), 
+                 facecolor='tab:red', alpha=0.4, edgecolor="tab:red")
+ax.fill_betweenx(y=np.linspace(np.min(y), np.median(y)), x1=np.median(x), x2=np.max(x), facecolor='tab:green', alpha=0.4)
+ax.text(0.4, 0.8,  
          r"$R^{2} = $" + f"{round(r_2,2)}", 
          horizontalalignment='center',
          verticalalignment='center', 
@@ -334,50 +341,5 @@ ax.text(0.2, -0.17,
 plt.show()
 
 # Empresas subvaloradas
-subvaloradas = industrias_empresas[(industrias_empresas['roe']>=np.median(x)) & (industrias_empresas['pb']<=np.median(y))].index.to_list()
+subvaloradas = industrias_empresas[(industrias_empresas['roic_wacc']>=np.median(x)) & (industrias_empresas['mv_bv']<=np.median(y))].index.to_list()
 print(f"Las empresas infravaloradas para el mercado chileno son: {subvaloradas}")
-
-
-# Caso EMPRESAS FINANCIERAS
-sector_ind = 'Consumer Defensive'
-x = industrias_empresas[industrias_empresas['sector']==sector_ind].mv_bv.values.flatten()
-y = industrias_empresas[industrias_empresas['sector']==sector_ind].roe_r.values.flatten()
-
-modelo = np.poly1d(np.polyfit(x, y, 1))
-r_2 = r2_score(y, modelo(x))
-
-fig, ax = plt.subplots(figsize=(10, 5))
-
-ax.scatter(x, y, color='tab:blue')
-ax.plot(x, modelo(x), color='tab:orange')
-fig.suptitle('Relación entre PB y el ROE', fontweight='bold')
-plt.title(f'Sector de {sector_ind} chileno')
-ax.set_ylabel('PB')
-ax.set_xlabel('ROE')
-# limites de valorización
-ax.axhline(y=np.median(y), color='tab:red', linestyle='dashed')
-ax.axvline(x=np.median(x), color='tab:red', linestyle='dashed')
-ax.text(0.8, 0.1,  
-         r"$R^{2} = $" + f"{round(r_2,2)}", 
-         horizontalalignment='center',
-         verticalalignment='center', 
-         transform=ax.transAxes, 
-         fontsize=24, 
-         color='black',
-         bbox=dict(facecolor='tab:gray', alpha=0.5))
-
-# Graph source
-ax.text(0.2, -0.17,  
-         "Fuente: EOD Historical Data   Gráfico: Lautaro Parada", 
-         horizontalalignment='center',
-         verticalalignment='center', 
-         transform=ax.transAxes, 
-         fontsize=8, 
-         color='black',
-         bbox=dict(facecolor='tab:gray', alpha=0.5))
-
-plt.show()
-
-# Empresas subvaloradas
-subvaloradas = industrias_empresas[(industrias_empresas['sector']==sector_ind) & (industrias_empresas['roe']>=np.median(x)) & (industrias_empresas['pb']<=np.median(y))].index.to_list()
-print(f"Las empresas infravaloradas para {sector_ind} chileno son: {subvaloradas}")
